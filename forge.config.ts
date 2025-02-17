7f032cfc46d77146ce219facb07a55474892b7a1
rconjoe <root@trog.codes>
2025-02-17 13:12:46 -0800

    working win32 builds

--- forge.config.ts
+++ forge.config.ts
@@ -15,7 +15,7 @@ const config: ForgeConfig = {
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
-    new MakerZIP({}, ["darwin"]),
+    new MakerZIP({}, ["win32"]),
     new MakerRpm({}),
     new MakerDeb({}),
   ],
