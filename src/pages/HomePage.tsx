8b9c13850bdcb09794fdb1f1610663412c1c8665
rconjoe <root@trog.codes>
2025-02-19 14:02:56 -0800

    remove icons

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ToggleTheme from "@/components/ToggleTheme";
 import { useTranslation } from "react-i18next";
 import Footer from "@/components/template/Footer";
-import InitialIcons from "@/components/template/InitialIcons";
 
 export default function HomePage() {
   const { t } = useTranslation();
@@ -10,7 +9,6 @@ export default function HomePage() {
   return (
     <div className="flex h-full flex-col">
       <div className="flex flex-1 flex-col items-center justify-center gap-2">
-        <InitialIcons />
         <span>
           <h1 className="font-mono text-4xl font-bold">{t("appName")}</h1>
           <p
