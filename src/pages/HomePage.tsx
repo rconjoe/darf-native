b1a602491e5d12a5aa0c1e07d5524c952064d120
rconjoe <root@trog.codes>
2025-02-19 14:00:00 -0800

    remove LangToggle

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ToggleTheme from "@/components/ToggleTheme";
 import { useTranslation } from "react-i18next";
-import LangToggle from "@/components/LangToggle";
 import Footer from "@/components/template/Footer";
 import InitialIcons from "@/components/template/InitialIcons";
 
@@ -14,11 +13,13 @@ export default function HomePage() {
         <InitialIcons />
         <span>
           <h1 className="font-mono text-4xl font-bold">{t("appName")}</h1>
-          <p className="text-end text-sm uppercase text-muted-foreground" data-testid="pageTitle">
+          <p
+            className="text-muted-foreground text-end text-sm uppercase"
+            data-testid="pageTitle"
+          >
             {t("titleHomePage")}
           </p>
         </span>
-        <LangToggle />
         <ToggleTheme />
       </div>
       <Footer />
