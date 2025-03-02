b21ee9ab1d971f46966ee19a2eb13992e4c1acc1
rconjoe <root@trog.codes>
2025-03-02 14:18:35 -0800

    lifted most of these functions

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,9 @@
-import { app, BrowserWindow } from "electron";
-import registerListeners from "./helpers/ipc/listeners-register";
-// "electron-squirrel-startup" seems broken when packaging with vite
-//import started from "electron-squirrel-startup";
-import path from "path";
+import { app } from "electron";
+import whenReady from "./helpers/app/events/when-ready";
 import {
   installExtension,
   REACT_DEVELOPER_TOOLS,
 } from "electron-devtools-installer";
-
-const inDevelopment = process.env.NODE_ENV === "development";
-
-function createWindow() {
-  const preload = path.join(__dirname, "preload.js");
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      devTools: inDevelopment,
-      contextIsolation: true,
-      nodeIntegration: true,
-      nodeIntegrationInSubFrames: false,
-
-      preload: preload,
-    },
-    titleBarStyle: "hidden",
-  });
-  registerListeners(mainWindow);
-
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(
-      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
-    );
-  }
-}
 
 async function installExtensions() {
   try {
@@ -45,18 +14,4 @@ async function installExtensions() {
   }
 }
 
-app.whenReady().then(createWindow).then(installExtensions);
-
-//osX only
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-//osX only ends
+app.whenReady().then(whenReady).then(installExtensions);
