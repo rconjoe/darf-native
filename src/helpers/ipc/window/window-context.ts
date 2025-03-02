069423af42e6d8636e6267599d08d54538f3dd7c
rconjoe <root@trog.codes>
2025-03-02 14:18:24 -0800

    comments on context

--- src/helpers/ipc/window/window-context.ts
+++ src/helpers/ipc/window/window-context.ts
@@ -4,6 +4,9 @@ import {
   WIN_CLOSE_CHANNEL,
 } from "./window-channels";
 
+// this function is called in preload.ts.
+// it has the special preload global window object.
+// handlers for these invocations are in the event listener files.
 export function exposeWindowContext() {
   const { contextBridge, ipcRenderer } = window.require("electron");
   contextBridge.exposeInMainWorld("electronWindow", {
@@ -12,3 +15,7 @@ export function exposeWindowContext() {
     close: () => ipcRenderer.invoke(WIN_CLOSE_CHANNEL),
   });
 }
+// this means we have window.electron.minimize etc available in frontend
+// it is the correct way to expose fcns, since you are exposing only
+// specific invocations of ipcRenderer on a particular channel, rather
+// than importing the entire ipcRenderer into the frontend (insecsure)
